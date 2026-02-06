f6b30b0d5ccf4d2fc05f64514c33989a0cc56605
Soham Ghadge <sohamghadge7900@gmail.com>
2026-02-06 23:06:26 +0530

    Resolve Supabase config and cleanup

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'EnterYourSupbaseURL'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'EnterYourSupabasePrivateKey'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-
-//database it not connected

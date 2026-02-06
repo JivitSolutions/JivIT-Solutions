4046f0abd3892796307cf6639b9cf2ac6d025dd6
Soham Ghadge <65226596+sohamghadge24@users.noreply.github.com>
2026-02-06 19:56:11 +0530

    Change Supabase URL and Anon Key defaults
    Updated Supabase URL and Anon Key placeholders for configuration.

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://yxqxxqmfazbrxpgchysp.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_RoBEdIRavImWb_akieit9Q_zM4SlwKi'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'EnterYourSupbaseURL'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'EnterYourSupabasePrivateKey'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 
-// Soham24052002@
+//database it not connected
